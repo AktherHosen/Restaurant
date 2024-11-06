6302c10e78305989fd00a087fe8ca5d6198f3255
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-11-06 23:44:12 +0600

    Banner responsive complete

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -4,30 +4,30 @@ import PrimaryButton from "../Button/PrimaryButton";
 import StarImg from "../../assets/star.png";
 const Banner = () => {
   return (
-    <div className="container mx-auto flex items-center h-[calc(100vh-80px)] relative">
-      <div className="space-y-4 relative z-20 rounded-lg lg:-mr-20">
-        <h1 className="text-4xl lg:text-6xl font-bold tracking-wider text-white bg-[#b72b1d] bg-opacity-50 inline-block  py-4 pr-4">
-          TASTE THE AUTHENTIC <br /> SAUDI CUISINE
+    <div className=" mx-auto flex flex-col lg:flex-row gap-6 lg:gap-0 items-center min-h-[calc(100vh-80px)] relative">
+      <div className="space-y-3 lg:space-y-4 relative z-20 rounded-lg mt-6 lg:mt-2 lg:-mr-20">
+        <h1 className="text-3xl lg:text-6xl lg:max-w-3xl font-bold tracking-wider text-white bg-[#b72b1d] bg-opacity-50 inline-block  lg:py-4 lg:pr-4">
+          TASTE THE AUTHENTIC SAUDI CUISINE
         </h1>
-        <p className="text-lg lg:text-xl text-white">
-          Among the best Saudi chefs in the world, serving <br /> you something
-          beyond flavor.
+        <p className="text-lg lg:text-xl text-white lg:max-w-xl">
+          Among the best Saudi chefs in the world, serving you something beyond
+          flavor.
         </p>
         <PrimaryButton label={"EXPLORE MENU"} />
       </div>
 
-      <div className="lg:w-1/2 hidden lg:block relative -ml-12 w-full">
+      <div className="lg:w-1/2  relative lg:-ml-12 w-full">
         <img
           src={BannerImg}
           alt="Saudi Cuisine"
           className="rounded-lg shadow-lg h-96 w-full object-cover"
         />
 
-        <div className="absolute -top-10 -right-8">
+        <div className="absolute -top-6 lg:-top-10 -right-4 lg:-right-8">
           <img src={StarImg} alt="" />
         </div>
-        <div className="bg-[#FEBF00] w-fit rounded-full p-1 text-black absolute -right-10 top-80">
-          <h1 className="border-dotted border-2 border-black uppercase rounded-full h-20 w-20 text-center  flex items-center font-bold">
+        <div className="bg-[#FEBF00] w-fit rounded-full p-1 text-black absolute -right-6 lg:-right-10 top-80">
+          <h1 className="border-dotted border-2 border-black uppercase rounded-full h-16 w-16 lg:h-20 lg:w-20 text-center text-xs lg:text-lg  flex items-center font-bold">
             Today Offer
           </h1>
         </div>
--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -5,7 +5,7 @@ import Container from "../Container/Container";
 
 const Header = () => {
   return (
-    <div className="bg-[#b72b1d] text-white min-h-[600px] ">
+    <div className="bg-[#b72b1d] text-white min-h-[600px] px-10 py-4">
       <Container>
         <Navbar />
         <Banner />
